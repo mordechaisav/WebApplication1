e27c0ad2ca92e967a2f5333dba07cd4682c1c8a5
Dell-pc <Dell-pc@FHGFD>
2024-07-25 17:26:03 +0300

    Add project files.

---
+++ WebApplication1/Controllers/CarController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: Car
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Model(int year, string name)
+        {
+            var car = new CarModel { Name = name,Year=year };
+            
+            return View(car);
+        }
+
+    }
+}
---
+++ WebApplication1/Models/CarModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class CarModel
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Name { get; set; }
+        public CarModel()
+        {
+            
+        }
+    }
+}
